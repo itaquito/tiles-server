d071a281f7f76b74c88e10febde7cf9932068a1b
itaquito <ichilaquiles@gmail.com>
2024-09-24 14:15:13 -0600

    feat: Adds dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config';
 import express from 'express';
 
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(express.static('tiles'));
 
-app.listen(3000, () => {
-  console.log(`[!] Express is running on port 3000`);
+app.listen(port, () => {
+  console.log(`[!] Express is running on port ${port}`);
 });
